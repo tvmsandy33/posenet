ed6e348924df2f9b4c98421be4a8bdc4ccda9422
tvmsandy33 <97993501+tvmsandy33@users.noreply.github.com>
2022-09-20 20:07:10 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,48 @@
+let capture;
+let posenet;
+let noseX, noseY;
+let singlepose;
+let skeleton;
+
+function setup() {
+    createCanvas(800,500);
+    capture = createCapture(VIDEO);
+    capture.hide();
+
+    posenet = ml5.poseNet(capture, modelLoaded);
+    posenet.on('pose', receivedPoses);
+
+}
+
+function receivedPoses(poses) {
+    console.log(poses);
+
+    if(poses.length > 0){
+        singlepose = poses[0].pose;
+        skeleton = poses[0].skeleton;
+
+    }
+}
+
+function modelLoaded() {
+    console.log('model has loaded');
+}
+
+function draw() {
+    background(200);
+    image(capture,0,0,400,400);
+    fill(0,0,255);
+    
+    if(singlepose) {
+        for(let i=0; i<singlepose.keypoints.length; i++) {
+            ellipse(singlepose.keypoints[i].position.x, singlepose.keypoints[i].position.y, 10,10);
+        }
+
+        stroke(255,255,255);
+        strokeWeight(5);
+        for(let j=0; j<skeleton.length; j++) {
+            p5.line(skeleton[j][0].x,skeleton[j][0].y,skeleton[j][1].x,skeleton[j][1].y);
+        }
+    }
+    
+}
